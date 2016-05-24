bb29e22924a38ba22c1c726c7e2dfd9ff6b07825
MoTao <mythly@qq.com>
2016-05-24 17:59:25 +0800

    fix a bug about modifiedState

--- others/poker.scala
+++ others/poker.scala
@@ -16,8 +16,8 @@ class Solver(n: Int) {
 						val part1 = state.slice(0, i - 1)
 						val part2 = Vector(state(i - 1) + 1, state(i) - 1)
 						val part3 = state.slice(i + 1, state.length - 1)
-						val part4 = Vector(i)
-						val modifiedState = part1 ++ part2 ++ part3 ++ part4						
+						val part4 = Vector(i - 1)
+						val modifiedState = part1 ++ part2 ++ part3 ++ part4
 						sum += choices * f(modifiedState)
 					}
 				}
@@ -27,7 +27,7 @@ class Solver(n: Int) {
 		}
 	}
 
-	def count = f(Vector(0, 0, 0, 0, n, -1))
+	def count = f(Vector(0, 0, 0, 0, n, 0))
 	def total = (1 to n * 4).map(BigInt(_)).reduce(_ * _)
 
 	var memory = Map[Vector[Int], BigInt]()
@@ -37,9 +37,11 @@ val solver = new Solver(13)
 println("Expected possibility = " + solver.count)
 println("Total possibility = " + solver.total)
 println("The propobility that no two adjacent cards share same number after a random shuffle = " + solver.count.toDouble / solver.total.toDouble)
+println("The propobility that at least two adjacent cards share same number after a random shuffle = " + (1 - solver.count.toDouble / solver.total.toDouble))
 
 /*
-Expected possibility = 981060862423245150073141067185004129776283019053858862120566784
+Expected possibility = 3668033946384704437729512814619767610579526911188666362431432294400
 Total possibility = 80658175170943878571660636856403766975289505440883277824000000000000
-The propobility that no two adjacent cards share same number after a random shuffle = 1.216319189398994E-5
+The propobility that no two adjacent cards share same number after a random shuffle = 0.045476282331094305
+The propobility that at least two adjacent cards share same number after a random shuffle = 0.9545237176689056
 */
