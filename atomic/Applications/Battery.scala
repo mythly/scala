7e04a2c183977a6be747d1fcfff03d3f56bc43b8
MoTao <mythly@qq.com>
2016-06-21 13:42:45 +0800

    Applications of atomic

---
+++ atomic/Applications/Battery.scala
@@ -0,0 +1,28 @@
+// Starter Code for Exercise 1
+// From "Traits" atom
+import com.atomicscala.AtomicTest._
+
+trait BatteryPower {
+	def monitor(power: Int): String
+}
+
+class EnergySource extends BatteryPower {
+	def monitor(power: Int) = {
+		power match {
+			case _ if power >= 40 => "green"
+			case _ if power >= 20 => "yellow"
+			case _ => "red"
+		}
+	}
+}
+
+class Battery extends EnergySource with BatteryPower
+
+object Battery extends App {
+	val battery = new Battery
+	battery.monitor(80) is "green"
+	battery.monitor(30) is "yellow"
+	battery.monitor(10) is "red"
+	args.foreach(arg => println(battery.monitor(arg.toInt)))	
+}
+
